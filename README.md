411a1a69ab4a4f31d743af495425d82d9e19749b
Kaspar Minosiants <kaspar.ru@gmail.com>
2021-01-31 23:07:38 +0300

    Block mode

--- README.md
+++ README.md
@@ -1,18 +1,56 @@
 ## crypto
 
- * [Java Security Developer's Gude](https://docs.oracle.com/en/java/javase/15/security/index.html)  
+ * [Java Security Developer's Gude](https://docs.oacle.com/en/java/javase/15/security/index.html)  
+ * [JDK JavaDoc](https://docs.oracle.com/en/java/javase/15/docs/api/index.html)  
  * [Bouncy Castle Home Page](https://www.bouncycastle.org/java.html)  
  * [Bouncy Castle source code](https://github.com/bcgit/bc-java)  
 
 ### [Java Cryptography Tools and Techniques](https://leanpub.com/javacryptotoolsandtech)  
 
+`OID` - object identifier  
+`CSOR` - compurer security objects register  
+`BER` - basic encoding rules  
+`DER` - distinguished encoding rules  
+`CER` - canonical encoding rules  
+`TLV` - tag length value  
+`OER` - octet encoding rules  
+`BOER` - basic objects encoding rules  
+`JCA` - java cryptography architecture   
+`JSE` - java security extention  
+`TLS` - transport layer security
+`SSL` - secure socket layer  
+`JSSE` - java security socket  
+`SPI` - service provider interface  
+`ECB` - Electronic Code Book  
+`CBC` - Cipher Block Chaining
+`IV` - Initialization vector
 #### Block and stream ciphers
 
 #### Symmetric cipher 
  * Secret key to encrypt data.  
  * At least 112 bits of security   
  * `NIST`, `AES` - recommended cipher
- *  
+   
+#### Block Modes 
+- `EBC` - Electornic Code Book. encrypt and decrypt data based on bits patterns in the book    
+- `CBC` - Cipher Block Chaining. 
+   - `IV` is used to initialise internal state and then XORing the state with each block of plane text    
+   - `Padding` - add an extra block of padding  
+   - `CTC` Cipher Text Stealing 
+- `Streaming`- encrypt data of an arbitrary size without the use of padding.  
+- `CTR` -   
+- `CFB` Cipher FeedBack - 
+- `OFB` Output FeedBack  
+
+#### Stream Ciphers   
+
+
+
+
+
+
+
+
 
 
 
--- src/main/scala/crypto/symetrickey.scala
+++ src/main/scala/crypto/symetrickey.scala
@@ -4,24 +4,77 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider
 import org.bouncycastle.util.encoders.Hex
 
 import java.security.Security
-import javax.crypto.{KeyGenerator, SecretKey}
-import javax.crypto.spec.SecretKeySpec
+import javax.crypto.{Cipher, KeyGenerator, SecretKey}
+import javax.crypto.spec.{IvParameterSpec, SecretKeySpec}
 
 object Symetric {
   Security.addProvider(new BouncyCastleProvider())
+  val iv = Hex.decode("9f741fdb5d8845bdb48a94394e84f8a3")
   //create secret key from byte array
-  def secretKey():SecretKey = {
-    val keyBytes = Hex.decode("000102030405060708090a0b0c0d0e0f")
-    new SecretKeySpec(keyBytes, "AES")
+  def secretKey(key: Array[Byte]): SecretKey = {
+    new SecretKeySpec(key, "AES")
   }
-  def genSecretKey():SecretKey = {
+
+  def genSecretKey(): SecretKey = {
     val kGen = KeyGenerator.getInstance("AES", "BC")
     kGen.generateKey()
   }
+
+  def ecbShortExample(input:Array[Byte], key:SecretKey) = {
+    // Electronic code book
+    val cipher = Cipher.getInstance("AES/ECB/NoPadding", "BC")
+    println("input    : " + Hex.toHexString(input))
+
+    cipher.init(Cipher.ENCRYPT_MODE, key)
+    val output = cipher.doFinal(input)
+    println("encrypted: " + Hex.toHexString(output))
+
+    cipher.init(Cipher.DECRYPT_MODE, key)
+    println("decrypted: " + Hex.toHexString(cipher.doFinal(output)))
+  }
+
+  def cbcExample(input:Array[Byte], key:SecretKey) = {
+    val cipher = Cipher.getInstance("AES/CBC/NoPadding", "BC")
+    println("input    : " + Hex.toHexString(input))
+    // or cipher.getIV
+    // or SecureRandom
+    //IV initialization vector
+    cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(iv))
+    val output = cipher.doFinal(input)
+    println("encrypted: " + Hex.toHexString(output))
+
+    cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv))
+    println("decrypted: " + Hex.toHexString(cipher.doFinal(output)))
+  }
+
+
+  def cbcPadExample(input:Array[Byte], key: SecretKey)={
+
+    val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding", "BC")
+    System.out.println("input    : " + Hex.toHexString(input))
+
+    cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(iv))
+    val output = cipher.doFinal(input)
+    System.out.println("encrypted: " + Hex.toHexString(output))
+
+    cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv))
+    val finalOutput = new Array[Byte](cipher.getOutputSize(output.length))
+    var len = cipher.update(output, 0, output.length, finalOutput, 0)
+    len += cipher.doFinal(finalOutput, len)
+   // System.out.println("decrypted: " + Hex.toHexString(Arrays.copyOfRange(finalOutput, 0, len)))
+  }
+  
 }
+
 object Main {
 
+  val keyBytes: Array[Byte] = Hex.decode("000102030405060708090a0b0c0d0e0f")
+  val input = Hex.decode("a0a1a2a3a4a5a6a7a0a1a2a3a4a5a6a7" + "a0a1a2a3a4a5a6a7a0a1a2a3a4a5a6a7")
+  val key = Symetric.secretKey(keyBytes)
+
   def main(args: Array[String]): Unit = {
-    println(Symetric.genSecretKey())
+    Symetric.ecbShortExample(input, key)
+    Symetric.cbcExample(input, key)
+    Symetric.cbcPadExample(input, key)
   }
 }
